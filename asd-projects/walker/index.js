fa8166ec6d3534cc723f971245bcaabecd68bacb
SierraLandry <scl0163546@lpssonline.com>
2021-06-08 19:20:38 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -1,79 +1,118 @@
 /* global $, sessionStorage */
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
+
 function runProgram(){
-  ////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////// SETUP /////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////// SETUP /////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
-  // Constant Variables
-  var FRAME_RATE = 60;
-  var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
-  // Game Item Objects
+// Constant Variables
+var FRAME_RATE = 60;
+var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+
+// Game Item Objects
 
 
-  // one-time setup
-  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('keydown', handleKeyDown); //todo1                          // change 'eventType' to the type of event you want to handle
+// one-time setup
+var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
+$(document).on('keydown', handleKeyDown); //todo1                          // change 'eventType' to the type of event you want to handle
+$(document).on('keyup', handleKeyUp); //todo6                          // change 'eventType' to the type of event you want to handle
 
-  ////////////////////////////////////////////////////////////////////////////////
-  ///////////////////////// CORE LOGIC ///////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
+var gameItem = {  //todo3//
+    positionX = 0, // the x-coordinate location for the box
+    speedX = 0, // the speed for the box along the x-axis
+    positionY = 0, // the x-coordinate location for the box
+    speedY = 0, // the speed for the box along the x-axis
+}
 
-  /* 
-  On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
-  by calling this function and executing the code inside.
-  */
-  function newFrame() {
-    
+////////////////////////////////////////////////////////////////////////////////
+///////////////////////// CORE LOGIC ///////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
-  }
-  
-  /* 
-  Called in response to events.
-  */
-  function handleKeyDown(event) { //todo1//
-      console.log(event); //todo1//
+/* 
+On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
+by calling this function and executing the code inside.
+*/
+function newFrame() { //given//
+  repositiongameItem(); //todo4 calling functions from below//
+  redrawgameItem();  //todo4 calling functions from below//
+}
 
-      if (event.which === KEYUP) { //todo2  supposed to call variable "up"//
-        console.log("up pressed");  //todo2  supposed to print "up pressed"//
-      }
-      else if (event.which === KEYLEFT) {  //todo2  supposed to call variable "left"//
-          console.log("left pressed");  //todo2  supposed to print "left pressed"//
-      }
-      else if (event.which === KEYRIGHT) {  //todo2  supposed to call variable "right"//
-            console.log("right pressed");  //todo2  supposed to print "right pressed"//
-      }
-      else if (event.which === KEYDOWN) {  //todo2  supposed to call variable "down"//
-              console.log("down pressed");  //todo2  supposed to print "down pressed"//
-      }
+/* 
+Called in response to events.
+*/
+function handleKeyDown(event) { //todo1//
+    console.log(event); //todo1//
+
+    if (event.which === KEY.UP) { //todo2  supposed to call variable "up"//
+      speedX = -5; //todo5//
+      //console.log("up pressed");  //todo2  supposed to print "up pressed"//
+    }
+    else if (event.which === KEY.LEFT) {  //todo2  supposed to call variable "left"//
+      speedX = -5; //todo5//
+      //console.log("left pressed");  //todo2  supposed to print "left pressed"//
+    }
+    else if (event.which === KEY.RIGHT) {  //todo2  supposed to call variable "right"//
+      speedX = -5;  //todo5//
+      //console.log("right pressed");  //todo2  supposed to print "right pressed"//
+    }
+    else if (event.which === KEY.DOWN) {  //todo2  supposed to call variable "down"//
+      speedX = -5;  //todo5//
+      //console.log("down pressed");  //todo2  supposed to print "down pressed"//
     }
   }
-  var KEYUP = { //todo2 meant to be variable for up key//
-    "UP": 38,  //todo2, number for up arrow//
-  }
-  var KEYLEFT = { //todo2 meant to be variable for left key//
-    "LEFT": 37,  //todo2, number for left arrow//
-  }
-  var KEYRIGHT = { //todo2 meant to be variable for right key//
-    "RIGHT": 39,  //todo2, number for right arrow//
-  }
-  var KEYDOWN = {  //todo2 meant to be variable for down key//
-    "DOWN": 40,  //todo2, number for down arrow//
-  }
-  ////////////////////////////////////////////////////////////////////////////////
-  ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
+/* 
+Called in response to events.
+*/
+  function handleKeyUp(event) { //todo6//
+    console.log(event); //todo6//
 
-  
-  function endGame() {
-    // stop the interval timer
-    clearInterval(interval);
-
-    // turn off event handlers
-    $(document).off();
+    if (event.which === KEY.UP) { //todo6  supposed to call variable "up"//
+      speedX = 0; //todo6 meant to stop box when key is released//
+      //console.log("up released");  //todo6  supposed to print "up released"//
+    }
+    else if (event.which === KEY.LEFT) {  //todo6  supposed to call variable "left"//
+      speedX = 0; //todo6 meant to stop box when key is released//
+      //console.log("left released");  //todo6  supposed to print "left released"//
+    }
+    else if (event.which === KEY.RIGHT) {  //todo6  supposed to call variable "right"//
+      speedX = 0;  //todo6 meant to stop box when key is released//
+      //console.log("right released");  //todo6  supposed to print "right released"//
+    }
+    else if (event.which === KEY.DOWN) {  //todo6  supposed to call variable "down"//
+      speedX = 0;  //todo6 meant to stop box when key is released//
+      //console.log("down released");  //todo6  supposed to print "down released"//
+    }
   }
-  
+
+}
+var KEY = { //todo2 meant to be variable for up key//
+  "UP": 38,  //todo2, number for up arrow//
+  "LEFT": 37,  //todo2, number for left arrow//
+  'RIGHT': 39,  //todo2, number for right arrow//
+  "DOWN": 40,  //todo2, number for down arrow//
+}
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+function repositiongameItem(){ //todo4//
+  positionX += speedX; // update the position of the box along the x-axis
+  positionY += speedY; // update the position of the box along the y-axis
+};
+
+function redrawgameItem(){ //todo4//
+  $("#gameItem").css("left", positionX);    // draw the box in the new location, positionX pixels away from the "left"
+  $("#gameItem").css("top", positionY);    // draw the box in the new location, positionX pixels away from the "top"
+};
+
+function endGame() {
+  // stop the interval timer
+  clearInterval(interval);
+
+  // turn off event handlers
+  $(document).off();
+}
+
 //}

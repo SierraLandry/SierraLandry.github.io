ec1ec40b33c24229b928e2e41ed3ac1a5e858638
SierraLandry <scl0163546@lpssonline.com>
2021-06-08 18:21:09 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -16,7 +16,7 @@ function runProgram(){
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown); //todo1                          // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -34,10 +34,35 @@ function runProgram(){
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
+  function handleKeyDown(event) { //todo1//
+      console.log(event); //todo1//
 
+      if (event.which === KEYUP) { //todo2  supposed to call variable "up"//
+        console.log("up pressed");  //todo2  supposed to print "up pressed"//
+      }
+      else if (event.which === KEYLEFT) {  //todo2  supposed to call variable "left"//
+          console.log("left pressed");  //todo2  supposed to print "left pressed"//
+      }
+      else if (event.which === KEYRIGHT) {  //todo2  supposed to call variable "right"//
+            console.log("right pressed");  //todo2  supposed to print "right pressed"//
+      }
+      else if (event.which === KEYDOWN) {  //todo2  supposed to call variable "down"//
+              console.log("down pressed");  //todo2  supposed to print "down pressed"//
+      }
+    }
   }
-
+  var KEYUP = { //todo2 meant to be variable for up key//
+    "UP": 38,  //todo2, number for up arrow//
+  }
+  var KEYLEFT = { //todo2 meant to be variable for left key//
+    "LEFT": 37,  //todo2, number for left arrow//
+  }
+  var KEYRIGHT = { //todo2 meant to be variable for right key//
+    "RIGHT": 39,  //todo2, number for right arrow//
+  }
+  var KEYDOWN = {  //todo2 meant to be variable for down key//
+    "DOWN": 40,  //todo2, number for down arrow//
+  }
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -51,4 +76,4 @@ function runProgram(){
     $(document).off();
   }
   
-}
+//}

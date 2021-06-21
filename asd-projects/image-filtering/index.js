be32c59207d5441c81a22282415215669a7e4663
SierraLandry <scl0163546@lpssonline.com>
2021-06-21 20:07:21 +0000

    saving image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -19,25 +19,23 @@ $(document).ready(function(){
 // TODO 1 & 3: Create the applyFilter function here
 function applyFilter(){
 
-    function loopOver(){ //step 2 todo 1
+   
         for (var r = 0; r < image.length; r++) {
             for (var c = 0; c < image[r].length; c++) {
-                var color = image[r][c];
+                var rgbString = image[r][c];
                 
-                var rgbString = r;
-                var rgbNumbers = rgbStringToArray(rgbString) [255, GREEN, BLUE
-                ];
-
+                
+                var rgbNumbers = rgbStringToArray(rgbString) 
+                rgbNumbers[RED] = 0;
+                rgbNumbers[GREEN] = 1;
+                rgbNumbers[BLUE] = 2;
+                
                 rbgArrayToString(rbgNumbers);
 
-                $("<div>").appendTo(element)
-                        .addClass("square")
-                        .css('left', c * SQUARE_SIZE)
-                        .css('top', r * SQUARE_SIZE)
-                        .css('background-color', color)
+            
             }
         }
-    }
+    
 }
 
 // TODO 5: Create the applyFilterNoBackground function
